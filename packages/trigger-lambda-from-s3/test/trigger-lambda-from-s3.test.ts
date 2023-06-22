848a6079e916f4bd93c80b5c3c0e68f7076f5647
kobakoba5884 <kobanminmin58@gmail.com>
2023-06-23 01:26:21 +0900

    add testcode and setting

--- packages/trigger-lambda-from-s3/test/trigger-lambda-from-s3.test.ts
+++ packages/trigger-lambda-from-s3/test/trigger-lambda-from-s3.test.ts
@@ -1,16 +1,63 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as TriggerLambdaFromS3 from '../lib/trigger-lambda-from-s3-stack';
+import { App } from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import { TriggerLambdaFromS3Stack } from '../lib/trigger-lambda-from-s3-stack';
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/trigger-lambda-from-s3-stack.ts
-test('SQS Queue Created', () => {
-  //   const app = new cdk.App();
-  //     // WHEN
-  //   const stack = new TriggerLambdaFromS3.TriggerLambdaFromS3Stack(app, 'MyTestStack');
-  //     // THEN
-  //   const template = Template.fromStack(stack);
-  //   template.hasResourceProperties('AWS::SQS::Queue', {
-  //     VisibilityTimeout: 300
-  //   });
-})
+test('Stack creates an S3 bucket', () => {
+  const app = new App();
+  const stack = new TriggerLambdaFromS3Stack(app, 'MyTestStack');
+  const template = Template.fromStack(stack);
+
+  template.hasResourceProperties('AWS::S3::Bucket', {
+    AccessControl: 'Private',
+    BucketEncryption: {
+      ServerSideEncryptionConfiguration: [
+        {
+          ServerSideEncryptionByDefault: {
+            SSEAlgorithm: 'aws:kms',
+          },
+        },
+      ],
+    },
+    PublicAccessBlockConfiguration: {
+      BlockPublicAcls: true,
+      BlockPublicPolicy: true,
+      IgnorePublicAcls: true,
+      RestrictPublicBuckets: true,
+    },
+  });
+});
+
+test('Stack creates a Lambda function', () => {
+  const app = new App();
+  const stack = new TriggerLambdaFromS3Stack(app, 'MyTestStack');
+  const template = Template.fromStack(stack);
+
+  template.hasResourceProperties('AWS::Lambda::Function', {
+    Handler: 'index.handler',
+    Runtime: 'nodejs18.x',
+    Timeout: 10,
+  });
+});
+
+test('Stack creates an IAM role', () => {
+  const app = new App();
+  const stack = new TriggerLambdaFromS3Stack(app, 'MyTestStack');
+  const template = Template.fromStack(stack);
+
+  template.hasResourceProperties('AWS::IAM::Role', {
+    AssumeRolePolicyDocument: {
+      Statement: [
+        {
+          Action: 'sts:AssumeRole',
+          Effect: 'Allow',
+          Principal: {
+            Service: 'lambda.amazonaws.com',
+          },
+        },
+      ],
+      Version: '2012-10-17',
+    },
+  });
+
+  template.resourceCountIs('AWS::IAM::Policy', 2);
+});
