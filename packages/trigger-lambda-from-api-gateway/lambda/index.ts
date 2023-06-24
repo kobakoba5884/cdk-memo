991574e988d721e2417bd5ed2da82048a56f4d82
kobakoba5884 <kobanminmin58@gmail.com>
2023-06-24 16:35:56 +0900

    add api-gateway project

---
+++ packages/trigger-lambda-from-api-gateway/lambda/index.ts
@@ -0,0 +1,54 @@
+import { ListObjectsV2Command, S3Client } from '@aws-sdk/client-s3'
+import { APIGatewayProxyResult, APIGatewayEvent } from 'aws-lambda'
+
+console.log('loading function')
+
+const bucketName = process.env.BUCKET
+const s3Client = new S3Client({})
+
+export const handler = async (
+  event: APIGatewayEvent
+): Promise<APIGatewayProxyResult> => {
+  console.log('Received event:', JSON.stringify(event, null, 2))
+
+  try {
+    const method = event.httpMethod
+
+    if (method === 'GET') {
+      if (event.path === '/') {
+        const command = new ListObjectsV2Command({
+          Bucket: bucketName,
+          MaxKeys: 1,
+        })
+
+        const response = await s3Client.send(command)
+
+        return {
+          statusCode: 200,
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(response.Contents),
+        }
+      } else {
+        return {
+          statusCode: 400,
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({ error: 'Invalid path' }),
+        }
+      }
+    } else {
+      return {
+        statusCode: 405,
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({ error: 'Invalid HTTP Method' }),
+      }
+    }
+  } catch (err) {
+    console.log(err)
+
+    return {
+      statusCode: 500,
+      headers: {'Content-Type': 'application/json' },
+      body: JSON.stringify({ error: 'Internal Server Error' }),
+    }
+  }
+}
---
+++ packages/trigger-lambda-from-api-gateway/lib/trigger-lambda-from-api-gateway-stack.ts
@@ -0,0 +1,105 @@
+import {
+  CfnOutput,
+  Duration,
+  RemovalPolicy,
+  Stack,
+  StackProps,
+} from 'aws-cdk-lib'
+import { Construct } from 'constructs'
+import {
+  Bucket,
+  BucketAccessControl,
+  BucketEncryption,
+  ObjectOwnership,
+  BlockPublicAccess,
+} from 'aws-cdk-lib/aws-s3'
+import {
+  Code,
+  Function as LambdaFunction,
+  Runtime,
+} from 'aws-cdk-lib/aws-lambda'
+import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway'
+
+const SHARED_NAME = 'trigger-lambda-from-api-gateway'
+
+export class TriggerLambdaFromApiGatewayStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props)
+
+    const bucket = this.createBucket()
+
+    const lambdaFucntion = this.createLambdaFunction(bucket)
+
+    bucket.grantWrite(lambdaFucntion)
+
+    const restApi = this.createApiGateway()
+
+    const lambdaIntegration = new LambdaIntegration(lambdaFucntion, {
+      requestTemplates: { 'application/json': '{ "statusCode": "200" }' },
+    })
+
+    restApi.root.addMethod('GET', lambdaIntegration)
+
+    this.createOutputs(bucket, lambdaFucntion, restApi)
+  }
+
+  createBucket = (): Bucket => {
+    const bucketName = `${SHARED_NAME}-bucket`
+
+    return new Bucket(this, bucketName, {
+      bucketName: bucketName,
+      versioned: false,
+      accessControl: BucketAccessControl.PRIVATE,
+      autoDeleteObjects: true,
+      removalPolicy: RemovalPolicy.DESTROY,
+      encryption: BucketEncryption.KMS_MANAGED,
+      objectOwnership: ObjectOwnership.OBJECT_WRITER,
+      blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+    })
+  }
+
+  createLambdaFunction = (bucket: Bucket): LambdaFunction => {
+    const functionName = `${SHARED_NAME}-function`
+
+    return new LambdaFunction(this, functionName, {
+      functionName: functionName,
+      code: Code.fromAsset('./dist/lambda'),
+      runtime: Runtime.NODEJS_18_X,
+      handler: 'index.handler',
+      timeout: Duration.seconds(10),
+      environment: {
+        BUCKET: bucket.bucketName,
+      },
+    })
+  }
+
+  createApiGateway = (): RestApi => {
+    const restApiName = `${SHARED_NAME}-rest-api`
+
+    return new RestApi(this, restApiName, {
+      restApiName: restApiName,
+      description: restApiName,
+    })
+  }
+
+  createOutputs = (
+    bucket: Bucket,
+    lambdaFunction: LambdaFunction,
+    restApi: RestApi
+  ): void => {
+    new CfnOutput(this, 'tableName', {
+      description: 'The name of the s3 bucket',
+      value: bucket.bucketName,
+    })
+
+    new CfnOutput(this, 'functionName', {
+      description: 'The name of the lambda function',
+      value: lambdaFunction.functionName,
+    })
+
+    new CfnOutput(this, 'restApiName', {
+      description: 'The name of the rest api gateway',
+      value: restApi.restApiName,
+    })
+  }
+}
