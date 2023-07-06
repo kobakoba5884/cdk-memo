baf5d34863b85593a983afa49f80fe9f1606cdcd
kobakoba5884 <kobanminmin58@gmail.com>
2023-07-06 16:44:43 +0900

    init cloud-formation-with-https project

---
+++ packages/cloud-formation-with-https/bin/cloud-formation-with-https.ts
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import { CloudFormationWithHttpsStack } from '../lib/cloud-formation-with-https-stack';
+
+const app = new cdk.App();
+new CloudFormationWithHttpsStack(app, 'CloudFormationWithHttpsStack', {
+});
