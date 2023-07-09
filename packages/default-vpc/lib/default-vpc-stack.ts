12aabbcae245860db1bc3b766184a29f9e765002
kobakoba5884 <kobanminmin5884@gmail.com>
2023-07-09 13:10:28 +0900

    not try to deploy default vpc

--- packages/default-vpc/lib/default-vpc-stack.ts
+++ packages/default-vpc/lib/default-vpc-stack.ts
@@ -1,16 +1,34 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Stack, StackProps } from 'aws-cdk-lib'
+import { SubnetType, Vpc } from 'aws-cdk-lib/aws-ec2'
+import { Construct } from 'constructs'
+import * as path from 'path'
 
-export class DefaultVpcStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+export class DefaultVpcStack extends Stack {
+  private readonly PROJECT_NAME: string = path.basename(
+    path.join(__dirname, '..')
+  )
 
-    // The code that defines your stack goes here
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props)
+  }
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'DefaultVpcQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+  createDefaultVpc = (): Vpc => {
+    return new Vpc(this, `${this.PROJECT_NAME}-vpc`, {
+      cidr: '10.0.0.0/16',
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: 'Ingress',
+          subnetType: SubnetType.PUBLIC,
+        },
+        {
+          cidrMask: 24,
+          name: 'Application',
+          subnetType: SubnetType.PRIVATE_WITH_EGRESS,
+        },
+      ],
+      enableDnsHostnames: true,
+      enableDnsSupport: true,
+    })
   }
 }
